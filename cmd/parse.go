cb4e22b976d63b68f3733e246b3d6da06fd9f5a5
Joel Jensen <yobert@gmail.com>
2019-03-12 10:56:48 -0600

    WIP finished up reparsing... just not sure how to do suffixes

--- cmd/parse.go
+++ cmd/parse.go
@@ -5,23 +5,41 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/pacur/pacur/pack"
 	"github.com/pacur/pacur/parse"
 )
 
 func ParsePkgBuild() (err error) {
+	var (
+		distro  string
+		release string
+	)
+
 	for i, path := range flag.Args() {
 		if i == 0 {
 			continue // skip "parse"
 		}
+		if i == 1 {
+			split := strings.Split(flag.Arg(1), "-")
+			distro = split[0]
+			release = ""
+			if len(split) > 1 {
+				release = split[1]
+			}
+			continue
+		}
+
 		fmt.Println(path)
-		pac := &pack.Pack{}
+		pac := &pack.Pack{
+			Distro:  distro,
+			Release: release,
+		}
 		pac.Init()
 		if err = parse.PkgBuild(pac, path); err != nil {
 			return
 		}
-		return
 		enc := json.NewEncoder(os.Stdout)
 		enc.SetIndent("", "   ")
 		enc.Encode(pac)
--- parse/parse.go
+++ parse/parse.go
@@ -1,20 +1,21 @@
 package parse
 
 import (
-	"bufio"
+	//	"bufio"
 	"bytes"
 	"context"
-	"fmt"
+	//	"fmt"
 	"github.com/dropbox/godropbox/errors"
+	"github.com/mitchellh/mapstructure"
 	"github.com/pacur/pacur/pack"
 	"github.com/pacur/pacur/utils"
 	"mvdan.cc/sh/v3/expand"
 	"mvdan.cc/sh/v3/shell"
 	"mvdan.cc/sh/v3/syntax"
-	"os"
+	//	"os"
 	"path/filepath"
 	"regexp"
-	"strings"
+	//	"strings"
 )
 
 const (
@@ -82,190 +83,56 @@ func PkgBuild(pac *pack.Pack, path string) (err error) {
 		return errors.Newf("parse: SourceNode on file %#v: %v", path, err)
 	}
 
-	/*		for key, val := range vars {
-			fmt.Printf("%#v\t", key)
-			switch val.Kind {
-			case expand.String:
-				fallthrough
-			case expand.NameRef:
-				fmt.Printf("%#v\n", val.Str)
-			case expand.Indexed:
-				fmt.Println()
-				for i, v := range val.List {
-					fmt.Printf("\t\t%d: %#v\n", i, v)
-				}
-			case expand.Associative:
-				fmt.Println()
-				for k, v := range val.Map {
-					fmt.Printf("\t\t%#v: %#v\n", k, v)
-				}
-			}
-		}*/
-	_ = expand.String
-	_ = vars
+	m := make(map[string]interface{})
 
-	funcDecls := map[string]string{}
+	for key, val := range vars {
+		switch val.Kind {
+		case expand.String:
+			fallthrough
+		case expand.NameRef:
+			m[key] = val.Str
+		case expand.Indexed:
+			m[key] = val.List
+		case expand.Associative:
+			m[key] = val.Map
+		default:
+			return errors.Newf("parse: Unhandled shell variable type %#v", val.Kind)
+		}
+	}
 
 	syntax.Walk(rootNode, func(node syntax.Node) bool {
 		switch x := node.(type) {
 		case *syntax.FuncDecl:
+			if x.Name == nil {
+				return false
+			}
 			cmd := x.Body.Cmd
 			if cmd == nil {
 				return false
 			}
-
 			block, ok := cmd.(*syntax.Block)
 			if !ok {
 				return false
 			}
-
-			buf := &bytes.Buffer{}
+			lines := []string{}
 			printer := syntax.NewPrinter()
 			for _, stmt := range block.Stmts {
-				printer.Print(buf, stmt)
-				fmt.Fprintln(buf)
+				var buf bytes.Buffer
+				printer.Print(&buf, stmt)
+				lines = append(lines, buf.String())
 			}
-			if x.Name == nil {
-				return false
-			}
-			funcDecls[x.Name.Value] = buf.String()
+			m[x.Name.Value] = lines
 		}
 		return true
 	})
 
-	for scanner.Scan() {
-		line := scanner.Text()
-		n += 1
+	// TODO Handle distro/release suffixed fields somehow.
+	// Not sure the best way to do this. I'd just run through and
+	// copy them here, but then shell variable substitution wouldn't
+	// work right. I'm going to think about a bash-ey way to do it.
 
-		if line == "" || line[:1] == "#" {
-			continue
-		}
-
-		if blockType == blockList {
-			if line == ")" {
-				for _, item := range itemReg.FindAllString(blockData, -1) {
-					blockItems = append(blockItems, item[1:len(item)-1])
-				}
-				err = pac.AddItem(blockKey, blockItems, n, line)
-				if err != nil {
-					return
-				}
-				blockType = 0
-				blockKey = ""
-				blockData = ""
-				blockItems = []string{}
-				continue
-			}
-
-			blockData += strings.TrimSpace(line)
-		} else if blockType == blockFunc {
-			if line == "}" {
-				err = pac.AddItem(blockKey, blockItems, n, line)
-				if err != nil {
-					return
-				}
-				blockType = 0
-				blockKey = ""
-				blockItems = []string{}
-				continue
-			}
-
-			blockItems = append(blockItems, strings.TrimSpace(line))
-		} else {
-			if strings.Contains(line, "() {") {
-				blockType = blockFunc
-				blockKey = strings.Split(line, "() {")[0]
-			} else {
-				parts := strings.SplitN(line, "=", 2)
-				if len(parts) != 2 {
-					err = &SyntaxError{
-						errors.Newf("parse: Line missing '=' (%d: %s)",
-							n, line),
-					}
-					return
-				}
-
-				key := parts[0]
-				val := parts[1]
-
-				if key[:1] == " " {
-					err = &SyntaxError{
-						errors.Newf("parse: Extra space padding (%d: %s)",
-							n, line),
-					}
-					return
-				} else if key[len(key)-1:] == " " {
-					err = &SyntaxError{
-						errors.Newf(
-							"parse: Extra space before '=' (%d: %s)",
-							n, line),
-					}
-					return
-				}
-
-				valLen := len(val)
-				switch val[:1] {
-				case `"`, "`":
-					if val[valLen-1:] != val[:1] {
-						err = &SyntaxError{
-							errors.Newf("parse: Unexpected char '%s' "+
-								"expected '%s' (%d: %s)",
-								val[valLen-1:], val[:1], n, line),
-						}
-						return
-					}
-
-					err = pac.AddItem(key, val[1:valLen-1], n, line)
-					if err != nil {
-						return
-					}
-				case "(":
-					if val[valLen-1:] == ")" {
-						if val[1:2] != `"` && val[1:2] != "`" {
-							err = &SyntaxError{
-								errors.Newf("parse: Unexpected char '%s' "+
-									"expected '\"' or '`' (%d: %s)",
-									val[1:2], n, line),
-							}
-							return
-						}
-
-						if val[valLen-2:valLen-1] != val[1:2] {
-							err = &SyntaxError{
-								errors.Newf("parse: Unexpected char '%s' "+
-									"expected '%s' (%d: %s)",
-									val[valLen-2:valLen-1], val[1:2],
-									n, line),
-							}
-							return
-						}
-
-						val = val[2 : len(val)-2]
-						err = pac.AddItem(key, []string{val}, n, line)
-						if err != nil {
-							return
-						}
-					} else {
-						blockType = blockList
-						blockKey = key
-					}
-				case " ":
-					err = &SyntaxError{
-						errors.Newf(
-							"parse: Extra space after '=' (%d: %s)",
-							n, line),
-					}
-					return
-				default:
-					err = &SyntaxError{
-						errors.Newf(
-							"parse: Unexpected char '%s' expected "+
-								"'\"' or '`' (%d: %s)", val[:1], n, line),
-					}
-					return
-				}
-			}
-		}
+	if err := mapstructure.Decode(m, &pac); err != nil {
+		return errors.Wrapf(err, "parse: Failed to map input structure to pack structure")
 	}
 
 	return
